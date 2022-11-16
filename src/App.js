ad630ffb0c13dd8d414cb208e4b3a676f0f300c8
ClrSundar <clr.sundar@gmail.com>
2022-11-16 18:13:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Dei this is my app.</p>
+    </div>
+  );
+}
+
+export default App;
